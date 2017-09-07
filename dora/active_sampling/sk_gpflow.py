d74a16059cbb71baa8efaf91f96de4d9193eb068
Dave Cole <david.cole@data61.csiro.au>
2017-09-07 17:42:48 +1000

    stash

--- dora/active_sampling/sk_gpflow.py
+++ dora/active_sampling/sk_gpflow.py
@@ -1,5 +1,5 @@
-import GPflow as gp
-from sklearn.base import BaseEstimator, RegressorMixin
+import gpflow as gp
+from sklearn.base import BaseEstimator, RegressorMixin, clone
 from sklearn.exceptions import NotFittedError
 from sklearn.utils.validation import check_is_fitted
 from sklearn.cluster import MiniBatchKMeans
@@ -33,7 +33,7 @@ def gaussian_quantiles(q):
 class SkGPflowRegressor(BaseEstimator, RegressorMixin):
     """ sklearn-like wrapper around GPflow regressors. """
 
-    def __init__(self, kern, **model_args):
+    def __init__(self, kern, optimize_args=None, **model_args):
         self.kern = kern
 
         default_args = {
@@ -51,11 +51,8 @@ class SkGPflowRegressor(BaseEstimator, RegressorMixin):
         self.model_args = model_args
         self.name = 'name'
 
-        self.optimize_args = {
-            'method': 'L-BFGS-B',
-            'maxiter': 10000
-        }
-
+        self.optimize_args = optimize_args or dict(method='L-BFGS-B',
+                                                   maxiter=10000)
         self.n_targets = None
 
     def _create_model(self, X, y):
@@ -67,6 +64,7 @@ class SkGPflowRegressor(BaseEstimator, RegressorMixin):
             msg = 'Dimensions of `x` ({}) do not match kernel dimensions ({})'
             raise ValueError(msg.format(X.shape[1], self.kern.input_dim))
 
+        y = np.atleast_2d(y)
         self.n_targets = y.shape[1]
 
         # create the GPflow model
@@ -94,7 +92,10 @@ class SkGPflowRegressor(BaseEstimator, RegressorMixin):
         return res
 
     def predict_quantiles(self, X, quantiles=None):
-        """ Return the predicted value y at defined quantiles. """
+        """ Return the predicted value y at defined quantiles.
+
+            output shape (n_samples, n_targets, n_quantiles)
+        """
         q = quantiles or [0.025, 0.975]
         samps = gaussian_quantiles(q)
         mu, var = self.model.predict_f(X)
@@ -122,7 +123,7 @@ class SkGPflowRegressor(BaseEstimator, RegressorMixin):
         res = self.model.optimize(**optimize_args)
         return res
 
-    def get_params(self, deep=False):
+    def get_params1(self, deep=False):
         """ Get GP parameters in sklearn form. Keyword arg `deep` is ignored."""
         check_is_fitted(self, 'model')
         p_dict = self.model.get_parameter_dict()
@@ -133,7 +134,7 @@ class SkGPflowRegressor(BaseEstimator, RegressorMixin):
         params = {sk_param_name(k): v for k, v in p_dict.items()}
         return params
 
-    def set_params(self, **params):
+    def set_params1(self, **params):
         """ Set GP params using sklearn form. """
         check_is_fitted(self, 'model')
         p_dict = self.model.get_parameter_dict()
@@ -147,7 +148,7 @@ class SkGPflowRegressor(BaseEstimator, RegressorMixin):
 
         self.model.set_parameter_dict(p_dict)
 
-    def __repr__(self):
+    def __repr__1(self):
         """ sklearn representation with GPflow model name. """
         class_name = self.__class__.__name__
         gp_class_name = self.gp_class.__module__ + "." + self.gp_class.__name__
@@ -162,9 +163,10 @@ class SkGPflowRegressor(BaseEstimator, RegressorMixin):
 
 
 class SkGPR(SkGPflowRegressor):
+    """ sklearn compatible wrapper for GPflow.gpr.GPR """
 
-    def __init__(self, kern, **model_args):
-        super().__init__(kern, **model_args)
+    def __init__(self, kern, optimize_args=None, **model_args):
+        super().__init__(kern, optimize_args, **model_args)
         self.gp_class = gp.gpr.GPR
 
     def _create_model(self, X, y):
@@ -182,9 +184,11 @@ class SkGPR(SkGPflowRegressor):
 
 
 class SkSGPR(SkGPflowRegressor):
+    """ sklearn compatible wrapper for GPflow.sgpr.SGPR """
 
-    def __init__(self, kern, inducing, random_seed=None, **model_args):
-        super().__init__(kern, **model_args)
+    def __init__(self, kern, inducing, random_seed=None, optimize_args=None,
+                 **model_args):
+        super().__init__(kern, optimize_args, **model_args)
         self.gp_class = gp.sgpr.SGPR
         self.inducing = inducing
         self.random_seed = random_seed
@@ -198,9 +202,10 @@ class SkSGPR(SkGPflowRegressor):
 
 
 class SkVGP(SkGPflowRegressor):
+    """ sklearn compatible wrapper for GPflow.vgp.VGP """
 
-    def __init__(self, kern, **model_args):
-        super().__init__(kern, **model_args)
+    def __init__(self, kern, optimize_args=None, **model_args):
+        super().__init__(kern, optimize_args, **model_args)
         self.gp_class = gp.vgp.VGP
 
     def _create_model(self, X, y):
@@ -208,9 +213,11 @@ class SkVGP(SkGPflowRegressor):
 
 
 class SkSVGP(SkGPflowRegressor):
+    """ sklearn compatible wrapper for GPflow.svgp.SVGP """
 
-    def __init__(self, kern, inducing, random_seed=None, **model_args):
-        super().__init__(kern, **model_args)
+    def __init__(self, kern, inducing, random_seed=None, optimize_args=None,
+                 **model_args):
+        super().__init__(kern, optimize_args, **model_args)
         self.gp_class = gp.svgp.SVGP
         self.inducing = inducing
         self.random_seed = random_seed
@@ -223,19 +230,58 @@ class SkSVGP(SkGPflowRegressor):
                                    minibatch_size=100, **self.model_args)
 
 
+class MultiTargetRegressor(BaseEstimator, RegressorMixin):
+    """ Simple version of sklearn.multioutput.MultiOutputRegressor without
+        parallel jobs to get around pickling problems with gpflow/tensorflow.
+    """
+
+    def __init__(self, estimator):
+        self.estimator = estimator
+        self.estimators_ = []
+
+    def fit(self, X, y,):
+        """ Create and fit a separate model for each output. """
+        def create_fit(X, yi):
+            est = clone(self.estimator)
+            est.fit(X, yi)
+            return est
+
+        self.estimators_ = [create_fit(X, y[:, i, np.newaxis]) for i in range(
+            y.shape[1])]
+
+    def predict(self, X, return_std=False, return_cov=False):
+        """ Run predict on each model and stack outputs. """
+        check_is_fitted(self, 'estimators_')
+
+        if return_std or return_cov:
+            raise NotImplementedError
+
+        res = np.hstack([e.predict(X) for e in self.estimators_])
+        return res
+
+    def predict_quantiles(self, X, quantiles=None):
+        """ Run predict_quantiles on each model and stack outputs. """
+        check_is_fitted(self, 'estimators_')
+
+        res = np.hstack([e.predict_quantiles(X, quantiles=quantiles) for e in
+                         self.estimators_])
+        return res
+
+
 class PipelineX(Pipeline):
+    """ Like sklearn.pipeline.Pipeline but with `y_transform` and
+        `predict_quantiles()`.
+    """
 
     def __init__(self, steps, y_transform=None):
         super().__init__(steps)
         self.y_transform = y_transform
-        self.n_targets = None
 
     def fit(self, X, y, **fit_model_kwargs):
         """ Fit y_transform and model. """
         if self.y_transform:
-            self.y_transform.fit(y)
+            self.y_transform.fit(y, None)
             y = self.y_transform.transform(y)
-            self.n_targets = y.shape[1]
         res = super().fit(X, y, **fit_model_kwargs)
         return res
 
@@ -259,12 +305,14 @@ class PipelineX(Pipeline):
         for name, transform in self.steps[:-1]:
             if transform is not None:
                 Xt = transform.transform(Xt)
+
         res = self.steps[-1][-1].predict(Xt, **final_est_kwargs)
 
         if self.y_transform and y_transform:
             if isinstance(res, tuple):
-                raise NotImplementedError
-            res = self.y_transform.inverse_transform(res)
+                res = tuple(map(self.y_transform.inverse_transform, res))
+            else:
+                res = self.y_transform.inverse_transform(res)
 
         return res
 
@@ -293,8 +341,89 @@ class PipelineX(Pipeline):
     def score(self, X, y=None):
 
         if self.y_transform:
-            self.y_transform.fit(y)
-            yt = self.y_transform.transform(y)
+            self.y_transform.fit(y, None)
+            y = self.y_transform.transform(y)
 
-        s = super().score(X, yt)
+        s = super().score(X, y)
         return s
+
+
+class CoregGP(SkGPflowRegressor):
+    def __init__(self, kern):  # , likelihood):
+        super().__init__(kern=kern)  # , likelihood=likelihood)
+        self.gp_class = gp.gpr.GPR
+
+    def _create_model(self, X, y):
+        self.input_dim = X.shape[1]
+        self.output_dim = y.shape[1]
+
+        # coregion kernel with random initial weights
+        coreg = gp.kernels.Coregion(1, output_dim=self.output_dim, rank=1,
+                                    active_dims=[self.input_dim])
+        coreg.W = np.random.randn(self.output_dim, 1)
+
+        kern = self.kern * coreg
+
+        X_a = self._coreg_data_transform_x(X)
+        y_a = self._coreg_data_transform_y(y)
+
+        from IPython import embed; embed()
+
+        self.model = self.gp_class(X_a, y_a, kern=kern, **self.model_args)
+
+        self.model.likelihood = likelihood_compute(self.model.likelihood)
+
+    def predict(self, X, return_std=False, return_cov=False):
+        X_a = self._coreg_data_transform_x(X)
+        res_a = super().predict(X_a, return_std=return_std,
+                                return_cov=return_cov)
+
+        if return_std or return_cov:
+            res = tuple(map(self._coreg_data_inverse_transform_y, res_a))
+        else:
+            res = self._coreg_data_inverse_transform_y(res_a)
+        return res
+
+    def predict_quantiles(self, X, quantiles=None):
+        X_a = self._coreg_data_transform_x(X)
+        yq_a = super().predict_quantiles(X_a, quantiles=quantiles)
+
+        yq_ar = np.rollaxis(yq_a, 2)
+        y_q = np.stack(
+            [self._coreg_data_inverse_transform_y(y) for y in yq_ar],
+            axis=2
+        )
+
+        #from IPython import embed; embed()
+        return y_q
+
+    def _coreg_data_transform_x(self, X):
+        """ Augment the input data X with an extra column containing indices
+            for each output dim.
+
+            For a model with N dependent variables:
+
+                X_a = [[X], [0],
+                       [X], [1],
+                       ..., ...,
+                       [X], [N]]
+
+                X_a.shape = (X.shape[0] * N, X.shape[1] + 1)
+        """
+        assert X.shape[1] == self.input_dim
+        X_a = np.vstack([np.hstack([X, np.full((X.shape[0], 1), i)]) for i in
+                         range(self.output_dim)])
+        return X_a
+
+    def _coreg_data_transform_y(self, y):
+        """ Reshape y data into a single column. """
+        assert y.shape[1] == self.output_dim
+        y_a = y.reshape((-1, 1), order='F')
+        return y_a
+
+    def _coreg_data_inverse_transform_y(self, y_a):
+        """ Reshape y data back into [n x m] where n is number of samples and
+            m is number of targets.
+        """
+        y = y_a.reshape((-1, self.output_dim), order='F')
+        return y
